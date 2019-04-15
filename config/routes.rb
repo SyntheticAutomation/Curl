eb745f9394e6bf751bdf111ae5258fa10a587b35
SyntheticAutomation <syntheticauto900@gmail.com>
2019-04-15 10:19:47 -0600

    Adds a few routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
   get '/', to: "home#index", as: 'root'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 
+  get '/history', to: 'searches#index'
 
 end
